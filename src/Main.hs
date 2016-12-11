7b58d625894cc88c8bf1b0d56b2e2d8fd494ecb9
Pascal Hartig <phartig@rdrei.net>
2016-12-11 13:37:22 +0000

    Qualify everything

--- src/Main.hs
+++ src/Main.hs
@@ -10,16 +10,24 @@
 module Main where
 
 import Protolude hiding ((<>))
+import Data.Monoid ((<>))
 
-import Data.Aeson
+-- Operator imports
+import Servant ((:<|>), (:>))
+
 import qualified Data.Aeson as Aeson
 import qualified Data.HashMap.Strict as HMS
-import Data.Monoid
-import Data.Text
-import Network.Wai
-import Network.Wai.Handler.Warp
+import qualified Data.Text as T
+import qualified Network.Wai as Wai
+import qualified Network.Wai.Handler.Warp as Warp
+import qualified Servant as Servant
 
-import Servant
+-- * Configuration
+
+data Configuration = Configuration
+  { confAppId :: Text
+  , confAppKey :: Text
+  , confStation :: Text }
 
 -- * Webhook Fulfillment Server
 
@@ -27,7 +35,7 @@ import Servant
 data WebhookRequest = WebhookRequest
   { result :: WebhookResult
   , id :: Text
-  } deriving (Generic, Show, FromJSON)
+  } deriving (Generic, Show, Aeson.FromJSON)
 
 data WebhookAction
   = ActionListDepartures
@@ -42,30 +50,30 @@ instance Aeson.FromJSON WebhookAction where
 data WebhookResult = WebhookResult
   { action :: Text
   , parameters :: WebhookParameters
-  } deriving (Generic, Show, FromJSON)
+  } deriving (Generic, Show, Aeson.FromJSON)
 
 newtype WebhookParameters =
   WebhookParameters (HMS.HashMap Text Text)
-  deriving (Generic, Show, FromJSON)
+  deriving (Generic, Show, Aeson.FromJSON)
 
 -- | A greet message data type
 newtype Greet = Greet { _msg :: Text }
-  deriving (Generic, Show, FromJSON, ToJSON)
+  deriving (Generic, Show, Aeson.FromJSON, Aeson.ToJSON)
 
 -- API specification
 type TestApi =
        -- GET /hello/:name?capital={true, false}  returns a Greet as JSON
-       "hello" :> Capture "name" Text :> QueryParam "capital" Bool :> Get '[JSON] Greet
+       "hello" :> Servant.Capture "name" Text :> Servant.QueryParam "capital" Bool :> Servant.Get '[Servant.JSON] Greet
 
        -- POST /greet with a Greet as JSON in the request body,
        --             returns a Greet as JSON
-  :<|> "greet" :> ReqBody '[JSON] Greet :> Post '[JSON] Greet
+  :<|> "greet" :> Servant.ReqBody '[Servant.JSON] Greet :> Servant.Post '[Servant.JSON] Greet
 
        -- Temporary
-  :<|> "webhook" :> ReqBody '[JSON] WebhookRequest :> Post '[JSON] Text
+  :<|> "webhook" :> Servant.ReqBody '[Servant.JSON] WebhookRequest :> Servant.Post '[Servant.JSON] Text
 
        -- DELETE /greet/:greetid
-  :<|> "greet" :> Capture "greetid" Text :> Delete '[JSON] NoContent
+  :<|> "greet" :> Servant.Capture "greetid" Text :> Servant.Delete '[Servant.JSON] Servant.NoContent
 
 testApi :: Proxy TestApi
 testApi = Proxy
@@ -76,29 +84,33 @@ testApi = Proxy
 -- that represents the API, are glued together using :<|>.
 --
 -- Each handler runs in the 'Handler' monad.
-server :: Server TestApi
-server = helloH :<|> postGreetH :<|> postWebhookH :<|> deleteGreetH
+server :: Servant.Server TestApi
+server = helloH Servant.:<|> postGreetH Servant.:<|> postWebhookH Servant.:<|> deleteGreetH
 
-  where helloH name Nothing = helloH name (Just False)
+  where helloH :: Monad m => Text -> Maybe Bool -> m Greet
+        helloH name Nothing = helloH name (Just False)
         helloH name (Just False) = return . Greet $ "Hello, " <> name
-        helloH name (Just True) = return . Greet . toUpper $ "Hello, " <> name
+        helloH name (Just True) = return . Greet . T.toUpper $ "Hello, " <> name
 
+        postGreetH :: Monad m => a -> m a
         postGreetH greet = return greet
 
+        postWebhookH :: Monad m => WebhookRequest -> m Text
         postWebhookH wh = return $ id wh
 
-        deleteGreetH _ = return NoContent
+        deleteGreetH :: Monad m => a -> m Servant.NoContent
+        deleteGreetH _ = return Servant.NoContent
 
 -- Turn the server into a WAI app. 'serve' is provided by servant,
 -- more precisely by the Servant.Server module.
-test :: Application
-test = serve testApi server
+test :: Servant.Application
+test = Servant.serve testApi server
 
 -- Run the server.
 --
 -- 'run' comes from Network.Wai.Handler.Warp
-runTestServer :: Port -> IO ()
-runTestServer port = run port test
+runTestServer :: Warp.Port -> IO ()
+runTestServer port = Warp.run port test
 
 -- Put this all to work!
 main :: IO ()
