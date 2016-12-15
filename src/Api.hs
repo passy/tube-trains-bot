bb9a28bd1bcbfb3dc17c634604881b131db34c43
Pascal Hartig <phartig@rdrei.net>
2016-12-15 22:28:50 +0000

    Style clean up, remove boilerplate

--- src/Api.hs
+++ src/Api.hs
@@ -5,7 +5,12 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
 
-module Api (loadDeparturesForStation) where
+module Api
+  ( loadDeparturesForStation
+  , Departure(..)
+  , Direction
+  , DepartureMap
+  ) where
 
 import Protolude hiding ((<>))
 import qualified Data.Aeson as Aeson
@@ -24,58 +29,71 @@ import Data.Aeson.Lens
 
 import Data.Aeson ((.:))
 
-data Direction = Westbound | Eastbound | Northbound | Southbound | Spellbound
+data Direction
+  = Westbound
+  | Eastbound
+  | Northbound
+  | Southbound
+  | Spellbound
   deriving (Show, Eq, Generic, Hashable.Hashable)
 
 instance Aeson.FromJSON Direction where
-  parseJSON (Aeson.String a) | a == "Westbound" = return Westbound
-                             | a == "Eastbound" = return Eastbound
-                             | a == "Northbound" = return Northbound
-                             | a == "Southbound" = return Northbound
-                             | otherwise = return Spellbound
+  parseJSON (Aeson.String a)
+    | a == "Westbound" = return Westbound
+    | a == "Eastbound" = return Eastbound
+    | a == "Northbound" = return Northbound
+    | a == "Southbound" = return Northbound
+    | otherwise = return Spellbound
   parseJSON _ = mempty
 
-
-data Departure = Departure { departureLine :: Text -- TODO: Should probably be a proper type.
-                           , departureDestination :: Text
-                           , departureSeconds :: Int
-                           }
-  deriving (Show, Eq)
+data Departure = Departure
+  { departureLine :: Text -- TODO: Should probably be a proper type.
+  , departureDestination :: Text
+  , departureSeconds :: Int
+  } deriving (Show, Eq)
 
 instance Aeson.FromJSON Departure where
-  parseJSON = Aeson.withObject "departure" $ \o ->
-    Departure <$> o .: "route_id"
-              <*> o .: "destination_name"
-              <*> o .: "time_seconds"
+  parseJSON =
+    Aeson.withObject "departure" $
+    \o ->
+       Departure <$> o .: "route_id" <*> o .: "destination_name" <*> o .:
+       "time_seconds"
 
-newtype DepartureMap = DepartureMap (HMS.HashMap Direction [Departure])
+newtype DepartureMap =
+  DepartureMap (HMS.HashMap Direction [Departure])
   deriving (Show, Eq)
 
 stationUrl :: Format.Format
-stationUrl = "https://citymapper.com/api/1/metrodepartures?headways=1&ids={}&region_id=uk-london"
+stationUrl =
+  "https://citymapper.com/api/1/metrodepartures?headways=1&ids={}&region_id=uk-london"
 
 mkUrlForStation :: Config.Config -> Maybe Text -> Text
-mkUrlForStation Config.Config{Config.defaultStation} stationName =
+mkUrlForStation Config.Config {Config.defaultStation} stationName =
   let station = fromMaybe (toStrict defaultStation) stationName
   in toStrict $ Format.format stationUrl (Format.Only station)
 
-loadDeparturesForStation :: MonadIO m => Config.Config -> Maybe Text -> m (Maybe DepartureMap)
+loadDeparturesForStation
+  :: MonadIO m
+  => Config.Config -> Maybe Text -> m (Maybe DepartureMap)
 loadDeparturesForStation config stationName = do
   let url = mkUrlForStation config stationName
   r <- liftIO . Wreq.get $ T.unpack url
   -- TODO: This is super fragile. I should at iterate and find the right depature groupings. The first
   -- part appears to be fixed. I'm sure there's some cool Lens shit for this.
   let groupings :: Maybe (Vector.Vector Aeson.Value)
-      groupings = r ^? Wreq.responseBody . key "stations" . nth 0 . key "sections" . nth 0 . key "departure_groupings" . _Array
+      groupings =
+        r ^? Wreq.responseBody
+           . key "stations"
+           . nth 0
+           . key "sections"
+           . nth 0
+           . key "departure_groupings"
+           . _Array
       departures :: Maybe (Vector.Vector (Direction, [Departure]))
       departures = sequence =<< fmap extractDepartures' <$> groupings
-
   return $ DepartureMap . HMS.fromList . Vector.toList <$> departures
-
   where
     extractDepartures :: Aeson.Value -> Aeson.Parser (Direction, [Departure])
-    extractDepartures = Aeson.withObject "departure" $ \o ->
-      (,) <$> o .: "direction_name" <*> o .: "departures"
-
+    extractDepartures = Aeson.withObject "departure" $ \o -> (,) <$> o .: "direction_name" <*> o .: "departures"
     extractDepartures' :: Aeson.Value -> Maybe (Direction, [Departure])
     extractDepartures' a = Aeson.parseMaybe extractDepartures a
--- src/Main.hs
+++ src/Main.hs
@@ -13,12 +13,11 @@ import Protolude hiding ((<>))
 import Data.Monoid ((<>))
 
 -- Operator imports
-import Servant ((:<|>)((:<|>)), (:>))
+import Servant ((:>))
 
 import qualified Data.Aeson as Aeson
 import qualified Data.Aeson.Types as Aeson
 import qualified Data.HashMap.Strict as HMS
-import qualified Data.Text as T
 import qualified Network.Wai.Handler.Warp as Warp
 import qualified Servant
 
@@ -58,26 +57,10 @@ newtype WebhookParameters =
   WebhookParameters (HMS.HashMap Text Text)
   deriving (Generic, Show, Aeson.FromJSON)
 
--- | A greet message data type
-newtype Greet = Greet { _msg :: Text }
-  deriving (Generic, Show, Aeson.FromJSON, Aeson.ToJSON)
-
 -- API specification
-type TestApi =
-       -- GET /hello/:name?capital={true, false}  returns a Greet as JSON
-       "hello" :> Servant.Capture "name" Text :> Servant.QueryParam "capital" Bool :> Servant.Get '[Servant.JSON] Greet
+type Api = "webhook" :> Servant.ReqBody '[Servant.JSON] WebhookRequest :> Servant.Post '[Servant.JSON] Text
 
-       -- POST /greet with a Greet as JSON in the request body,
-       --             returns a Greet as JSON
-  :<|> "greet" :> Servant.ReqBody '[Servant.JSON] Greet :> Servant.Post '[Servant.JSON] Greet
-
-       -- Temporary
-  :<|> "webhook" :> Servant.ReqBody '[Servant.JSON] WebhookRequest :> Servant.Post '[Servant.JSON] Text
-
-       -- DELETE /greet/:greetid
-  :<|> "greet" :> Servant.Capture "greetid" Text :> Servant.Delete '[Servant.JSON] Servant.NoContent
-
-testApi :: Proxy TestApi
+testApi :: Proxy Api
 testApi = Proxy
 
 -- Server-side handlers.
@@ -86,25 +69,14 @@ testApi = Proxy
 -- that represents the API, are glued together using :<|>.
 --
 -- Each handler runs in the 'Handler' monad.
-server :: Config.Config -> Servant.Server TestApi
-server c = helloH :<|> postGreetH :<|> postWebhookH :<|> deleteGreetH
+server :: Config.Config -> Servant.Server Api
+server c = postWebhookH
 
-  where helloH :: Monad m => Text -> Maybe Bool -> m Greet
-        helloH name Nothing = helloH name (Just False)
-        helloH name (Just False) = return . Greet $ "Hello, " <> name
-        helloH name (Just True) = return . Greet . T.toUpper $ "Hello, " <> name
-
-        postGreetH :: Monad m => a -> m a
-        postGreetH greet = return greet
-
-        postWebhookH :: MonadIO m => WebhookRequest -> m Text
+  where postWebhookH :: MonadIO m => WebhookRequest -> m Text
         postWebhookH wh = do
           res <- Api.loadDeparturesForStation c (Just "AldgateEast")
           print res
           return $ _id wh
-
-        deleteGreetH :: Monad m => a -> m Servant.NoContent
-        deleteGreetH _ = return Servant.NoContent
 
 -- Turn the server into a WAI app. 'serve' is provided by servant,
 -- more precisely by the Servant.Server module.
