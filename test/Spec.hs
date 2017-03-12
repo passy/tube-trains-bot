25b08e9cde4195edcbdedc6faaeb296d971ba7b5
Pascal Hartig <phartig@rdrei.net>
2017-03-12 15:55:44 +0000

    Add test for single and multi departures

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,14 +1,17 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications #-}
 
 import Protolude
 import System.Directory (getCurrentDirectory )
 import System.FilePath ((</>))
 
-import qualified Data.Aeson             as Aeson
-import qualified Data.ByteString.Lazy   as BS
-import qualified Data.Text.Encoding     as TE
-import qualified Data.Text.IO           as TIO
+import qualified Data.Aeson as Aeson
+import qualified Data.Aeson.Types as Aeson
+import qualified Data.ByteString.Lazy as BS
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as TE
+import qualified Data.Text.IO as TIO
 
 import qualified Api
 
@@ -25,5 +28,15 @@ main = hspec $ do
     describe "JSON Parsing" $ do
 
       it "parses a search response" $ do
+        pendingWith "soon"
         resp <- readFixture "metrodepartures.json"
         Api.parseDepartures resp `shouldSatisfy` isJust
+
+      it "prases a departure" $ do
+        resp <- readFixture "singledeparture.json"
+        let dp :: Api.Departure
+            dp = either (error . T.pack) identity (Aeson.eitherDecode resp)
+
+        Api.departureLine dp `shouldBe` "District"
+        Api.departureDestination dp `shouldBe` "West Ham"
+        Api.departureSeconds dp `shouldBe` 187
