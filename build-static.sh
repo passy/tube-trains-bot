95a25cfc49a67ce1c5789dc5038cf08da5ae9a2c
Pascal Hartig <phartig@rdrei.net>
2018-12-11 10:12:37 +0000

    Update static build script

--- build-static.sh
+++ build-static.sh
@@ -2,7 +2,7 @@
 
 set -eux
 
-LAST_LINE=$(stack sdist 2>&1 | tail -n 1)
+LAST_LINE=$(stack sdist 2>&1 | grep '\.tar\.gz$')
 SDIST=${LAST_LINE##* }
 
 rm -rf build-static
@@ -12,6 +12,6 @@ docker run --rm \
     -v $(pwd)/build-static:/host-bin \
     -v $SDIST:/sdist.tar.gz \
     -v $(pwd)/build-home:/home/build \
-    fpco/docker-static-haskell:8.0.2 \
+    passy/docker-haskell-static:latest \
     /bin/bash -c \
     'chown $(id -u) $HOME && rm -rf $HOME/src && mkdir $HOME/src && cd $HOME/src && tar zxfv /sdist.tar.gz && cd * && stack install --system-ghc --test --local-bin-path /host-bin --ghc-options "-optl-static -fPIC -optc-Os" && upx --best --ultra-brute /host-bin/*'
